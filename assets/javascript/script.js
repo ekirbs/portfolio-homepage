a0fb198926f87a29cdfd38e74187514d3c6c607c
Eric K <erickirberger@gmail.com>
2022-11-23 21:11:53 -0500

    removed js folder

--- assets/javascript/script.js
+++
@@ -1,7 +0,0 @@
-let input = document.getElementById("send-button");
-    input.addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-            event.preventDefault();
-            document.getElementById("send-button").click();
-        }
-    });
